51bc0f0d0f36d0b163dd6a34fb69cdbdb73545e2
Victoria  Topham <topham.victoria@gmail.com>
2020-06-17 09:05:24 -0700

    moving_zerioes test passes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,19 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
+    
+    # When you encounter a 0, remove it and move it to the end. if that happens, keep the tracking index the same as another element has shifted to take its place
+    i = 0
+    stop_index = len(arr)
+    while i < stop_index:
+        if arr[i] == 0:
+            arr.append(arr.pop(i))
+            #we don't want to infinite loop!
+            stop_index -= 1
+        else:
+            i+= 1
+    print(arr)
+    return arr
 
 
 if __name__ == '__main__':
