b95ba6468356ec779e03eea6cc353f219f9d6fee
Victoria  Topham <topham.victoria@gmail.com>
2020-06-17 14:03:14 -0700

    fixed a typo

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,18 +3,16 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n, tracking = 0):
-    print(f"n is {n}")
     if n < 2:
         return 1
 
     num1 = 0
 
     for i in range(1, 4):
-        if tracking + i == n:
-            
+        if tracking + i == n:    
             return num1 + 1
         elif tracking + i < n:
-            num1 = eating_cookies(n, tracking + i)
+            num1 += eating_cookies(n, tracking + i)
     return num1
 
 if __name__ == "__main__":
